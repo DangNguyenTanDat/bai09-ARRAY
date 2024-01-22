1f92600f8b94568a664711d5712afea83ac3e717
DangNguyenTanDat <156651386+DangNguyenTanDat@users.noreply.github.com>
2024-01-22 14:18:39 +0700

    Add files via upload

---
+++ index4.php
@@ -0,0 +1,8 @@
+<?php
+$array = array(
+    'amzuni.com',
+    'website học lập trình online'
+);
+print_r(array_pop($array));
+// output:2
+?>
---
+++ index5.php
@@ -0,0 +1,6 @@
+<?php
+$array = [5, 4, 3, 2, 1];
+sort($array); //True
+print_r($array);
+// output: array ([0]=>1 [1]=>2 [2]=>3 [3]=>4 [4]=>5)
+?>
